3cd45e065ede07c7c2ec721d2b5abe3ccd927e12
onlyyasad <onlyyasad@gmail.com>
2023-05-19 12:51:15 +0600

    Get api for category generated

--- index.js
+++ index.js
@@ -14,24 +14,43 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const toysCollection = client.db('kiddozDB').collection('toys');
+
+        app.get('/toys', async (req, res) => {
+            const result = await toysCollection.find().toArray();
+            res.send(result)
+        })
+        app.get('/toys/:sub_category', async (req, res) => {
+            const sub_category = req.params.sub_category;
+            if (sub_category === "All Cars") {
+                const result = await toysCollection.find().toArray();
+                res.send(result)
+            }
+            else {
+                const result = await toysCollection.find({ sub_category: sub_category }).toArray();
+                res.send(result)
+            }
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
@@ -40,6 +59,6 @@ app.get('/', (req, res) => {
     res.send("Sports Kid Server is running")
 });
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`This server is running on port: ${port}`)
 })
